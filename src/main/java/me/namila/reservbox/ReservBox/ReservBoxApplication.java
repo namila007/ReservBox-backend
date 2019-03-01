d63f729ac8ccfad134bf339360d41a7d5ed88386
Namila Bandara <namila007@gmail.com>
2019-03-01 16:10:03 +0530

    init :dart:

---
+++ src/main/java/me/namila/reservbox/ReservBox/ReservBoxApplication.java
@@ -0,0 +1,15 @@
+package me.namila.reservbox.ReservBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservBoxApplication
+{
+
+	public static void main( String[] args )
+	{
+		SpringApplication.run( ReservBoxApplication.class, args );
+	}
+
+}
