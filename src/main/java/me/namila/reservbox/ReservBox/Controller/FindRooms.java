7774417bce2310bd9f29fb3f7b33e8f1089a1134
Namila Bandara <namila007@gmail.com>
2019-03-12 19:00:50 +0530

    room search
    Signed-off-by: Namila Bandara <namila007@gmail.com>

---
+++ src/main/java/me/namila/reservbox/ReservBox/Controller/FindRooms.java
@@ -0,0 +1,11 @@
+package me.namila.reservbox.ReservBox.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class FindRooms
+{
+
+}
