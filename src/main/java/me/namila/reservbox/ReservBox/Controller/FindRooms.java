8c23402c3d644a083319ca7a030fd0cd720d9c0e
Namila Bandara <namila007@gmail.com>
2019-03-13 15:47:22 +0530

    room search working
    Signed-off-by: Namila Bandara <namila007@gmail.com>

--- src/main/java/me/namila/reservbox/ReservBox/Controller/FindRooms.java
+++ src/main/java/me/namila/reservbox/ReservBox/Controller/FindRooms.java
@@ -1,8 +1,8 @@
 package me.namila.reservbox.ReservBox.Controller;
 
-import me.namila.reservbox.ReservBox.Model.Contract;
 import me.namila.reservbox.ReservBox.Repository.ContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -20,27 +17,16 @@ import java.util.List;
 @RequestMapping("/api/search")
 public class FindRooms
 {
-    final DateFormat format = new SimpleDateFormat("YYYY-MM-DD");
 
     @Autowired
     private ContractRepository contractRepository;
 
-//    @GetMapping
-//    private ResponseEntity<?> status() {
-//        return new ResponseEntity<>("Okay",HttpStatus.OK);
-//    }
-
-    @GetMapping()
-    private ResponseEntity<?> getContract(@RequestParam("adults") int adults, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
-        Date start = null, end = null;
-        try {
-            start = format.parse(startDate);
-            end = format.parse(endDate);
-            System.out.println(start);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return new ResponseEntity<List<Contract>>(contractRepository.getSearch(adults, start, end), HttpStatus.OK);
+	@GetMapping()
+	private ResponseEntity<?> getContract( @RequestParam("adults") int adults,
+			@RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+			@RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate )
+	{
+		List<Object[]> data = contractRepository.getSearch( adults, startDate, endDate );
+		return new ResponseEntity<List<Object[]>>( data, HttpStatus.OK );
     }
 }
--- src/main/java/me/namila/reservbox/ReservBox/Repository/ContractRepository.java
+++ src/main/java/me/namila/reservbox/ReservBox/Repository/ContractRepository.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 public interface ContractRepository extends CrudRepository<Contract, Integer>
 {
-    @Query("SELECT distinct c.rooms , c.hotel from  Contract c  inner join c.hotel h inner join c.rooms r where r.maxAdults >= :adults and c.startDate <= :startDate and c.endDate >= :endDate")
-    List<Contract> getSearch(@Param("adults") int adults,
+	@Query("SELECT distinct h.name, r.roomType ,r.roomRate from  Contract c  inner join c.hotel h "
+			+ "inner join c.rooms r where r.maxAdults >= :adults"
+			+ " and c.startDate <= :startDate and c.endDate >= :endDate")
+	List<Object[]> getSearch( @Param("adults") int adults,
                              @Param("startDate") Date startDate,
                              @Param("endDate") Date endDate);
 }
